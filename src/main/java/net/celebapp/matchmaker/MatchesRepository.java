285a64e1ae26d71a54b2864090abb72032a5c178
laurenshareshian <laurenshareshian@gmail.com>
2020-11-23 20:27:04 -0500

    added unlike route

--- src/main/java/net/celebapp/matchmaker/MatchesRepository.java
+++ src/main/java/net/celebapp/matchmaker/MatchesRepository.java
@@ -13,8 +13,16 @@ import java.util.List;
 
 	@Modifying
 	@Transactional
-	@Query(value = "INSERT INTO matches (fk_profile_id, fk_dream_profile_id, message_to_dream_profile) VALUES (?1, ?2, ?3);", nativeQuery = true)
+	@Query(value = "INSERT INTO matches (fk_profile_id, fk_dream_profile_id, message_to_dream_profile) " +
+			"VALUES (?1, ?2, ?3);", nativeQuery = true)
 	void addMatch(Integer profileId, Integer dreamProfileId, String message);
+
+	@Modifying
+	@Transactional
+	@Query(value = "DELETE FROM matches WHERE fk_profile_id=?1 AND fk_dream_profile_id=?2", nativeQuery = true)
+	void unlike(Integer profileId, Integer dreamProfileId);
+
+	MatchesEntity findByFkProfileIdAndFkDreamProfileId(Integer profileId, Integer dreamProfileId);
 
 	}
 
